31c724d1efc9e5a82d1271364cced655786c4cc3
Alejo Aragon <alejo.aragon.05@gmail.com>
2022-09-10 15:50:18 -0500

    update_project

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -19,7 +19,7 @@ def do_deploy(archive_path):
         If the file doesn't exist at archive_path or an error occurs - False.
         Otherwise - True.
     """
-    if os.path.isfile(archive_path) is False:
+    if os.path.exists(archive_path) is False:
         return False
     file = archive_path.split("/")[-1]
     name = file.split(".")[0]
