74a13a62cdffd54513dff316fb184e624147d21b
Alejo Aragon <alejo.aragon.05@gmail.com>
2022-09-10 16:57:37 -0500

    update_project

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,7 +4,7 @@ Fabric script based on the file 1-pack_web_static.py that distributes an
 archive to the web servers
 """
 from os.path import exists
-from fabric.api import env, put, run
+from fabric.api import put, run
 
 env.hosts = ["54.175.45.148", "52.207.249.170"]
 
@@ -24,10 +24,10 @@ def do_deploy(archive_path):
         run("sudo mkdir -p {}{}".format(path, unfile))
         run("sudo tar -xzf /tmp/{} -C {}{}".format(filename, path, unfile))
         run("sudo rm -rf /tmp/{}".format(filename))
-        run('sudo mv {0}{1}/web_static/* {0}{1}/'.format(path, unfile))
-        run('sudo rm -rf {}{}/web_static'.format(path, unfile))
-        run('sudo rm -rf /data/web_static/current')
-        run('sudo ln -s {}{}/ /data/web_static/current'.format(path, unfile))
+        run("sudo mv {0}{1}/web_static/* {0}{1}/".format(path, unfile))
+        run("sudo rm -rf {}{}/web_static".format(path, unfile))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s {}{}/ /data/web_static/current".format(path, unfile))
         print("New version deployed!")
         return True
     except Exception:
