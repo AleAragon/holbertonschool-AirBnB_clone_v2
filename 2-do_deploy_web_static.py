1b45a7941ebd3ae0abd328951d3eebb95096c937
Alejo Aragon <alejo.aragon.05@gmail.com>
2022-09-10 18:16:36 -0500

    update_project

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -10,25 +10,21 @@ env.hosts = ["54.175.45.148", "52.207.249.170"]
 
 
 def do_deploy(archive_path):
-    """
-    Distributes an archive to your web servers
-    Returns False if the file at the path archive_path doesn't exist
-    """
+    """distributes an archive to the web servers"""
     if exists(archive_path) is False:
         return False
     try:
-        filename = archive_path.split("/")[-1]
-        unfile = filename[0:-4]
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
         path = "/data/web_static/releases/"
-        put(archive_path, "/tmp/")
-        run("sudo mkdir -p {}{}".format(path, unfile))
-        run("sudo tar -xzf /tmp/{} -C {}{}".format(filename, path, unfile))
-        run("sudo rm -rf /tmp/{}".format(filename))
-        run("sudo mv {0}{1}/web_static/* {0}{1}/".format(path, unfile))
-        run("sudo rm -rf {}{}/web_static".format(path, unfile))
-        run("sudo rm -rf /data/web_static/current")
-        run("sudo ln -s {}{}/ /data/web_static/current".format(path, unfile))
-        sudo('service nginx restart')
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
     except Exception:
         return False
